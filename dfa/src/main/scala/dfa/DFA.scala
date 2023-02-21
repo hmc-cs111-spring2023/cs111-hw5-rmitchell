e509d37773c2f767c3ba8caf8818b4eaa3c4d917
Ryder Mitchell <60051227+rmitchellPitzer@users.noreply.github.com>
2023-02-21 23:35:16 +0000

    Statrting on hw

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -1,3 +1,26 @@
 package dfa // leave this line in the file
 
 // TODO: replace this comment with your implementation
+
+case class State(label: String):
+
+  def label: String = label
+//   def minutes: Int = (amount / 60) % 60
+//   def hours:   Int = (amount / 3600).toInt
+
+//   override def toString = f"$hours%02d:$minutes%02d:$seconds%02d"
+
+//   def +(other: Stopwatch) = Stopwatch(amount + other.amount)
+//   def +(moreSeconds: Int) = Stopwatch(amount + moreSeconds)
+
+case class Transition():
+    def from: State = 
+    def symbol: Char = 
+    def to: State = 
+
+class DFA():
+    def states: Set[State] = 
+    def transitions: Set[Transition] = 
+    def startState: State = 
+    def acceptingStates: Set[State] = 
+        
