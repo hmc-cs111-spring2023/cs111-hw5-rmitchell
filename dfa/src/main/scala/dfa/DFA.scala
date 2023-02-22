499cc0bab94b30c50c54d9699cd9eb0509d17ead
Ryder Mitchell <60051227+rmitchellPitzer@users.noreply.github.com>
2023-02-22 00:18:32 +0000

    continuing on hw

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -2,25 +2,14 @@ package dfa // leave this line in the file
 
 // TODO: replace this comment with your implementation
 
-case class State(label: String):
+case class State (label:String)
+case class Transition (from:State, symbol:Char, to:State)
 
-  def label: String = label
-//   def minutes: Int = (amount / 60) % 60
-//   def hours:   Int = (amount / 3600).toInt
-
-//   override def toString = f"$hours%02d:$minutes%02d:$seconds%02d"
-
-//   def +(other: Stopwatch) = Stopwatch(amount + other.amount)
-//   def +(moreSeconds: Int) = Stopwatch(amount + moreSeconds)
-
-case class Transition():
-    def from: State = 
-    def symbol: Char = 
-    def to: State = 
-
-class DFA():
-    def states: Set[State] = 
-    def transitions: Set[Transition] = 
-    def startState: State = 
-    def acceptingStates: Set[State] = 
-        
+class DFA(states:Set[State], transitions:Set[Transition],
+startState: State, acceptingStates:Set[State]):
+    def accepts(input:String): Boolean = {
+    input match {
+    case "" => True
+    case _ => 
+  }
+}
