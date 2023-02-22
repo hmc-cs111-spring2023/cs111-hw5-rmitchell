69ba08df8417ee03a4dea3677e0a95a658fc9ac3
Ryder Mitchell <60051227+rmitchellPitzer@users.noreply.github.com>
2023-02-22 01:22:40 +0000

    Getting there

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -3,13 +3,22 @@ package dfa // leave this line in the file
 // TODO: replace this comment with your implementation
 
 case class State (label:String)
-case class Transition (from:State, symbol:Char, to:State)
+case class Transition (from:State, to:State, symbol:Char)
 
 class DFA(states:Set[State], transitions:Set[Transition],
-startState: State, acceptingStates:Set[State]):
-    def accepts(input:String): Boolean = {
+start: State, accept:Set[State]):
+    
+  def accepts(input:String): Boolean = {
     input match {
-    case "" => True
-    case _ => 
+    case "" => 
+      if (accept.contains(this.start)){return true} 
+      else {return false}
+    case stringInput:Any =>
+    //   val newList = (this.transitions.filter(_.from == this.startState)).filter(_.symbol == stringInput.charAt(0))
+      val newList = this.transitions.filter(_.from == this.start).filter(_.symbol == stringInput.charAt(0))
+      val newStartState:State = (newList.head).to	
+      val recursiveDFA:DFA = DFA(this.states, this.transitions, newStartState, this.accept)
+      return recursiveDFA.accepts(stringInput.substring(1))
+      
   }
 }
